41f774fb0edb48d8cecebfe2cb21412b5f243ae1
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-12 10:55:26 +0300

    edit layout

--- portfolio-website/js/script.js
+++ portfolio-website/js/script.js
@@ -1,7 +1,19 @@
-const navLinks = document.querySelectorAll('.nav li a');
-navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-        navLinks.forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navLinks.forEach(link => link.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    smoothLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 });
